a28b16848553b8afd02a10c68c298b2006f71e2d
Manoj E S <manoj.es.professional@gmail.com>
2023-09-19 02:24:27 +0530

    Implement ros2 runtime parameters

--- src/my_py_pkg/my_py_pkg/number_publisher.py
+++ src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -12,10 +12,21 @@ class NumberPublisherNode(Node):
     def __init__(self):
         super().__init__("number_publisher")
 
-        self.__number = 143
+        # Declare runtime parameters
+        # `ros2 run <pkg_name> <exec_name> --ros-args -p <param1_name>:=value1 -p <param2_name>:=value2`
+        self.declare_parameter("number_to_publish", 143)
+        self.declare_parameter("publish_frequency", 0.5)
+
+        # Get parameter value at runtime
+        self.__number = self.get_parameter("number_to_publish").value
+        NumberPublisherNode.publish_frequency = self.get_parameter("publish_frequency").value
+
         self.__logger = self.get_logger()
         self.__publisher = self.create_publisher(Int64, "number", 10)
-        self.__timer = self.create_timer(0.5, self.publish_number)      # 2Hz publish rate
+        self.__timer = self.create_timer(
+            (1/NumberPublisherNode.publish_frequency),
+            self.publish_number
+        )
 
         self.__logger.info(f'Number Publisher Publishing {self.__number}')
 
